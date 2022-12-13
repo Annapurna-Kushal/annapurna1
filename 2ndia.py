c914fe1cb09e4a7065d2df5901915414114b8f56
Annapurna-Kushal <119652327+Annapurna-Kushal@users.noreply.github.com>
2022-12-13 06:46:26 +0530

    Update 2ndia.py

--- 2ndia.py
+++ 2ndia.py
@@ -1,4 +1,26 @@
-x=10
-y=20
-c=x+y
-print(c)
+def LinearSearch(array, n, k):
+
+    for j in range(0, n):
+
+        if (array[j] == k):
+
+            return j
+
+    return -1
+
+ 
+array = [1, 3, 5, 7, 9]
+
+k = 7
+n = len(array)
+
+result = LinearSearch(array, n, k)
+
+if(result == -1):
+
+    print("Element not found")
+
+else:
+
+    print("Element found at index: ", result)
+ 
